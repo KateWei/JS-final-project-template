2942ee858d5c439d0c68485c6d012c35b7a142cf
KateWei <katewei1258@gmail.com>
2016-06-25 17:34:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ bgImg.src = "images/map.png";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
-setTimeout(draw,1000)
 function draw(){
   ctx.drawlmage(bglmg,0,0);
 }
+setTimeout(draw,1000)
