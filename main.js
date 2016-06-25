601b895afd6cb346d64e9700fb6eae86dcc51de6
KateWei <katewei1258@gmail.com>
2016-06-25 17:51:54 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ bgImg.src = "images/map.png";
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 var bgImg = document.createElement("img");
-bgImg.src = "";
+bgImg.src = "https://github.com/KateWei/jsGameByOa/blob/gh-pages/images/daigh.gif?raw=true";
 
 function draw(){
   ctx.drawImage(bgImg,0,0);
