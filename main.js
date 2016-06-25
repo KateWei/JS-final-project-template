c67f2d95910f36aa85dfa71e739af2d8e0b6894f
KateWei <katewei1258@gmail.com>
2016-06-25 17:42:43 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 
 function draw(){
-  ctx.drawlmage(bglmg,0,0);
+  ctx.drawlmage(bgImg,0,0);
 }
 setTimeout(draw,1000)
