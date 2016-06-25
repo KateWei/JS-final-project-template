000b09318418362855621162583bfda90d200a5f
KateWei <katewei1258@gmail.com>
2016-06-25 17:30:46 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+var bgImg = document.createElement("img");
+bgImg.src = "images/map.png";
+var canvas = document.getElementById("game-canvas");
+var ctx = canvas.getContext("2d");
+
 function draw(){
   ctx.drawlmage(bglmg,0,0);
 }
