042900ed2a820a7d7015bde998c9c9a1f314b7f8
KateWei <katewei1258@gmail.com>
2016-07-30 16:54:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ var hero = {
 };
 
 varSlimeImg = document.createElement("img")
-slimeImg.src = "images/slime.gif"
+slimeImg.src = "images/slime.png"
 
 var heroImg = document.createElement("img");
 heroImg.src = "images/rukia.gif";
