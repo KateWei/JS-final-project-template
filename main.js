09eee75dab612f57b4f0e03ca7bd4a291ad9de08
KateWei <katewei1258@gmail.com>
2016-09-03 14:48:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -162,7 +162,7 @@ function draw(){
         var hp = 100
         ctx.font = "24px Arial";
         ctx.fillStyle = "white";
-        ctx.fillText("TreeHP",20,20);
+        ctx.fillText("TreeHP",20,30);
 
 }
 
