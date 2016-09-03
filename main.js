57053a7170d338e9e133880a375f46c18f704099
KateWei <katewei1258@gmail.com>
2016-09-03 17:20:30 +0800

    Update main.js

--- main.js
+++ main.js
@@ -148,10 +148,8 @@ var tower = {
         	        	return;
         	        }
                 }
-             }
         	//如果都沒找到，會進道這行，清除鎖定的目標
-        	this.imingEnemyId = null;
-        	},
+        	this.imingEnemyId = null;,
         };
 
 function draw(){
