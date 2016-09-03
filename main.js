29bb355648483f879eac5a28dc72b8b2184ce97c
KateWei <katewei1258@gmail.com>
2016-09-03 17:15:25 +0800

    Update main.js

--- main.js
+++ main.js
@@ -129,7 +129,7 @@ var tower = {
         			var distance = Math.sqrt(
         				Math.pow(this.x-enemies[1].x,2) + Math.pow(this.y-enemies[1].y,2))
         	};
-        	shoot: function(id){
+        	shoot: function shoot(id){
         		ct.beginPath();
         		ctx.moveTo(this.x + 16, this.y);
         		ctx.lineTo(enemies[id].x + 16, enemies[id].y + 16);
